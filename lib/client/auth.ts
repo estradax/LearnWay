81d325d89ef6a2be621e9a47be40e08c3fbee26e
estradax <azharumuhammad@gmail.com>
2025-08-10 19:25:16 +0700

    updatet

--- lib/client/auth.ts
+++ lib/client/auth.ts
@@ -3,5 +3,6 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { auth } from "@/lib/server/auth";
 
 export const { signIn, signUp, useSession, signOut } = createAuthClient({
+  baseURL: '',
   plugins: [inferAdditionalFields<typeof auth>()],
 });
